64a9c0ecf2abd746883a9958db949b986b69bbe2
Ryan Yim <yknyim@gmail.com>
2019-05-06 12:08:46 -0400

    allowed editting of draft text

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-function NotesEditor({text}) {
+function NotesEditor({text, handleChange}) {
     return (
-        <textarea value={text} />
+        <textarea value={text} onChange={(e) => {
+            handleChange(e.target.value);
+        }} />
     );
 }
 
@@ -10,21 +12,41 @@ class NotesDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditting: false
+            isEditting: false,
+            draftText: props.note.text
         };
     }
     render() {
         // declares the note variable and assigns them to the properties of this.props where the name matches.
         const {note} = this.props;
-        const {isEditting} = this.state;
+        const {isEditting, draftText} = this.state;
         return (
             <div>
                 {
-                    isEditting ? <NotesEditor text={note.text} /> : note.text
+                    isEditting ? <NotesEditor 
+                                    handleChange={this._changeDraftText} 
+                                    text={draftText}
+                                    /> 
+                            : draftText
                 }
+                <br />
+                <button onClick={this._toggleIsEditing}>Toggle</button>
             </div>
         );
     }
+
+    _toggleIsEditing = () => {
+        this.setState({
+            isEditting: !this.state.isEditting
+        });
+    }
+
+    _changeDraftText = (newText) => {
+        this.setState({
+            draftText: newText
+        });
+    }
+
 }
 
 

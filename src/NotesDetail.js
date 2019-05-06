8b51a8e5dcaf964bd21cc8dc8e8f00acc4c5fdee
Ryan Yim <yknyim@gmail.com>
2019-05-06 11:52:25 -0400

    adds editing feature to NotesDetail

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
+function NotesEditor({text}) {
+    return (
+        <textarea value={text} />
+    );
+}
+
 class NotesDetail extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            isEditting: false
+        };
     }
     render() {
         // declares the note variable and assigns them to the properties of this.props where the name matches.
-        const {note} = this.props
+        const {note} = this.props;
+        const {isEditting} = this.state;
         return (
             <div>
-                {note.text}
+                {
+                    isEditting ? <NotesEditor text={note.text} /> : note.text
+                }
             </div>
         );
     }
